4b483317a92357ce11a1dd274b69192eacd839d2
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2017-05-26 19:03:12 +0400

    bump version, fix scorrers logging

--- R/als_implicit.R
+++ R/als_implicit.R
@@ -147,6 +147,7 @@ ALS_implicit = R6::R6Class(
           trace_iter[[j]] = list(iter = i, scorer = sc, value = score)
           j = j + 1L
         }
+        trace_iter = data.table::rbindlist(trace_iter)
         trace_lst[[i]] = data.table::rbindlist(list(trace_iter, list(iter = i, scorer = "loss", value = loss)))
         flog.info("iter %d loss = %.4f %s", i, loss, trace_scors_string)
         #------------------------------------------------------------------------
