eb7656b97e89d3446e1ab945f73bf91b90aa1378
dselivanov <selivanov.dmitriy@gmail.com>
2017-09-10 15:03:29 +0400

    fast grid-search cross-validation for linear-flow

--- R/linear-flow.R
+++ R/linear-flow.R
@@ -9,75 +9,163 @@
 #'   model = LinearFlow$new( rank = 8L,
 #'                           lambda = 0,
 #'                           svd_solver = c("irlba", "randomized_svd"),
-#'                           orthogonal_basis = NULL)
+#'                           Q = NULL)
 #'   model$fit_transform(x, ...)
 #'   model$predict(x, k, n_threads = 1L, not_recommend = x, ...)
 #'   model$components
-#'   model$orthogonal_basis
+#'   model$Q
+#'   model$cv = function(x, x_cv_train, x_cv_cv, lambdas = "auto@@50", metric = "map@@10",
+#'                       n_threads = 1L, not_recommend = x_cv_train, ...)
 #' }
 #' @export
 LinearFlow = R6::R6Class(
   classname = "LinearFlow",
   public = list(
+    Q = NULL,
+    components = NULL,
     initialize = function(rank = 8L,
                           lambda = 0,
                           svd_solver = c("irlba", "randomized_svd"),
-                          orthogonal_basis = NULL
+                          Q = NULL
     ) {
       private$rank = rank
       private$svd_solver = match.arg(svd_solver)
       private$lambda = lambda
-      self$orthogonal_basis = orthogonal_basis
+      self$Q = Q
     },
     fit_transform = function(x, ...) {
+      stopifnot(!is.null(colnames(x)))
+      private$item_ids = colnames(x)
+      self$Q = private$calc_Q(x, ...)
+      flog.debug("calculating RHS")
+      rhs = t(self$Q) %*% t(x) %*% x
+      flog.debug("calculating LHS")
+      lhs = rhs %*% self$Q
+      self$components = private$fit_transform_internal(lhs, rhs, private$lambda, ...)
+      invisible(as.matrix(x %*% self$Q))
+    },
+    cv = function(x, x_cv_train, x_cv_cv, lambdas = "auto@50", metric = "map@10",
+                  n_threads = 1L, not_recommend = x_cv_train, ...) {
 
+      stopifnot(!is.null(colnames(x)))
       private$item_ids = colnames(x)
 
-      if(!is.null(self$orthogonal_basis)) {
-        stopifnot(nrow((self$orthogonal_basis)) == ncol(x))
-        stopifnot(ncol((self$orthogonal_basis)) == private$rank)
-      } else
-      if(is.null(self$orthogonal_basis)) {
-        if(private$svd_solver == "irlba") {
-          flog.debug("fitting truncated SVD with irlba")
-          trunc_svd = irlba::irlba(x, nv = private$rank, tol = 1e-4)
+
+      stopifnot(private$item_ids == colnames(x_cv_cv))
+      stopifnot(private$item_ids == colnames(x_cv_train))
+
+      lambda_auto = FALSE
+      if(is.character(lambdas)) {
+        if (length(grep(pattern = "(auto)\\@[[:digit:]]+", x = lambdas)) != 1 )
+          stop(sprintf("don't know how add '%s' metric 'auto@k' or numeric are supported", lambdas))
+        lambdas = strsplit(lambdas, "@", T)[[1]]
+        lambdas_k = as.integer(lambdas[[2]])
+        lambda_auto = TRUE
+      } else {
+        stopifnot(is.numeric(lambdas))
+      }
+
+      if (length(grep(pattern = "(ndcg|map)\\@[[:digit:]]+", x = metric)) != 1 )
+        stop(sprintf("don't know how add '%s' metric. Only 'map@k', 'ndcg@k' are supported", metric))
+      metric = strsplit(metric, "@", T)[[1]]
+      metric_k = as.integer(metric[[2]])
+      metric_name = metric[[1]]
+
+      self$Q = private$calc_Q(x, ...)
+      flog.info("calculating RHS")
+      rhs = t(self$Q) %*% t(x) %*% x
+      flog.info("calculating LHS")
+      lhs = rhs %*% self$Q
+      # calculate "reasonable" lambda from values of main diagonal of LSH
+      if(lambda_auto) {
+        lhs_diag = diag(lhs)
+        # generate sequence of lambdas
+        lambdas = seq(log10(0.1 * min(lhs_diag)), log10(100 * max(lhs_diag)), length.out = lambdas_k)
+        lambdas = 10 ^ lambdas
+      }
+
+      cv_res = data.frame(lambda = lambdas, score = NA_real_)
+      xq_cv_train = as.matrix(x_cv_train %*% self$Q)
+
+      for(i in seq_along(lambdas)) {
+        lambda = lambdas[[i]]
+        Y = private$fit_transform_internal(lhs, rhs, lambda, ...)
+        preds = private$predict_internal(xq_cv_train, k = metric_k, Y = Y, n_threads = n_threads, not_recommend = not_recommend)
+        score = NULL
+        if(metric_name == "map")
+          score = mean(ap_k(preds, x_cv_cv, ...), na.rm = T)
+        if(metric_name == "ndcg")
+          score = mean(ndcg_k(preds, x_cv_cv, ...), na.rm = T)
+
+        cv_res$score[[i]] = score
+        if(score >= max(cv_res$score, na.rm = T) || is.null(self$components)) {
+          self$components = Y
+          private$lambda = lambda
         }
-        if(private$svd_solver == "randomized_svd") {
-          flog.debug("fitting truncated SVD with randomized algorithm")
-          trunc_svd = irlba::svdr(x, private$rank)
-        }
-        self$orthogonal_basis = trunc_svd$v
-      } else
-        stop(sprintf("don't know %s", private$svd_solver))
-      flog.debug("calculating RHS")
-      rhs = t(self$orthogonal_basis) %*% t(x) %*% x
-      flog.debug("calculating LHS")
-      lhs = rhs %*% self$orthogonal_basis + Diagonal(private$rank, private$lambda)
-      flog.debug("solving least squares")
-      self$components = as.matrix(solve(lhs, rhs))
-      list(lhs = self$orthogonal_basis, rsh = self$components)
+        flog.info("%d/%d lambda %.3f score = %.3f", i, length(lambdas), lambda, score)
+      }
+      cv_res
     },
     predict = function(x, k, n_threads = 1L, not_recommend = x, ...) {
-
-      user_item_score = x %*% self$orthogonal_basis %*% self$components
-      user_item_score = as.matrix(user_item_score)
-      indices = top_k_indices_byrow(user_item_score, k, n_threads, not_recommend)
-      scores = attr(indices, "scores", exact = TRUE)
-      attr(indices, "scores") = NULL
-      predicted_item_ids = private$item_ids[indices]
-      data.table::setattr(predicted_item_ids, "dim", dim(indices))
-      data.table::setattr(predicted_item_ids, "indices", indices)
-      data.table::setattr(predicted_item_ids, "scores", scores)
-      data.table::setattr(predicted_item_ids, "dimnames", list(rownames(x), NULL))
+      xq = x %*% self$Q
+      predicted_item_ids = private$predict_internal(xq, k = k, self$components, n_threads = n_threads, not_recommend = not_recommend, ...)
       predicted_item_ids
-    },
-    orthogonal_basis = NULL,
-    components = NULL
+    }
   ),
   private = list(
     rank = NULL,
     svd_solver = NULL,
     lambda = NULL,
-    item_ids = NULL
+    item_ids = NULL,
+    calc_Q = function(x, ...) {
+      result = NULL
+      if(!is.null(self$Q)) {
+        flog.debug("found Q, checking it...")
+        stopifnot(nrow((self$Q)) == ncol(x))
+        stopifnot(ncol((self$Q)) == private$rank)
+        result = self$Q
+      } else {
+        if(is.null(self$Q)) {
+          if(private$svd_solver == "irlba") {
+            flog.info("fitting truncated SVD with irlba")
+            trunc_svd = irlba::irlba(x, nv = private$rank, tol = 1e-4)
+          } else {
+            if(private$svd_solver == "randomized_svd") {
+              flog.info("fitting truncated SVD with randomized algorithm")
+              trunc_svd = irlba::svdr(x, private$rank)
+            } else
+                stop(sprintf("don't know %s", private$svd_solver))
+          }
+        }
+        result = trunc_svd$v
+      }
+      stopifnot(is.numeric(result))
+      result
+    },
+    fit_transform_internal = function(lsh, rhs, lambda, ...) {
+      flog.debug("solving least squares with lambda %.3f", lambda)
+      lhs_ridge = lsh + Diagonal(private$rank, lambda)
+      as.matrix(solve(lhs_ridge, rhs))
+    },
+    predict_internal = function(xq, k, Y, n_threads = 1L, not_recommend = x, ...) {
+      # user_item_score = x %*% self$Q %*% Y
+      user_item_score = xq %*% Y
+
+      if(!is.matrix(user_item_score))
+        user_item_score = as.matrix(user_item_score)
+
+      flog.debug("predicting top %d values", k)
+      indices = top_k_indices_byrow(user_item_score, k, n_threads, not_recommend)
+      scores = attr(indices, "scores", exact = TRUE)
+      data.table::setattr(indices, "scores", NULL)
+
+      # predicted_item_ids = colnames(x)[indices]
+      predicted_item_ids = private$item_ids[indices]
+      data.table::setattr(predicted_item_ids, "dim", dim(indices))
+      data.table::setattr(predicted_item_ids, "indices", indices)
+      data.table::setattr(predicted_item_ids, "scores", scores)
+      data.table::setattr(predicted_item_ids, "dimnames", list(rownames(xq), NULL))
+      predicted_item_ids
+    }
   )
 )
