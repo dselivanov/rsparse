98c893c8002622ebc7c2b489e189c6792cd5a6a8
dselivanov <selivanov.dmitriy@gmail.com>
2017-11-23 12:26:07 +0400

    combine code bases for soft-impute and soft-svd

---
+++ R/SoftALS.R
@@ -0,0 +1,181 @@
+soft_impute = function(x,
+                       rank = 10L, lambda = 0,
+                       n_iter = 100L, convergence_tol = 1e-3,
+                       init = NULL, final_svd = TRUE) {
+  soft_als(x,
+          rank = rank, lambda = lambda,
+          n_iter = n_iter, convergence_tol = convergence_tol,
+          init = init, final_svd = final_svd,
+          target = "soft_impute")
+}
+
+soft_svd = function(x,
+                    rank = 10L, lambda = 0,
+                    n_iter = 100L, convergence_tol = 1e-3,
+                    init = NULL, final_svd = TRUE) {
+  soft_als(x,
+           rank = rank, lambda = lambda,
+           n_iter = n_iter, convergence_tol = convergence_tol,
+           init = init, final_svd = final_svd,
+           target = "svd")
+}
+
+# workhorse for soft_impute
+solve_iter_als_softimpute = function(x, svd_current, lambda, singular_vectors = c("u", "v")) {
+  singular_vectors = match.arg(singular_vectors)
+
+  if(singular_vectors == "v") {
+    A = t(svd_current$u) * sqrt(svd_current$d)
+    B = t(svd_current$v) * sqrt(svd_current$d)
+  } else {
+    A = t(svd_current$v) * sqrt(svd_current$d)
+    B = t(svd_current$u) * sqrt(svd_current$d)
+  }
+
+  x_delta = x
+  # make_sparse_approximation calculates values of sparse matrix X_new = X - A %*% B
+  # for only non-zero values of X
+  x_delta@x = x@x - make_sparse_approximation(x, A, B)
+  loss = (as.numeric(crossprod(x_delta@x)) + lambda * sum(svd_current$d)) / length(x_delta@x)
+
+  first = (x_delta %*% svd_current[[singular_vectors]]) %*% diag( sqrt(svd_current$d) / (svd_current$d + lambda))
+  rm(x_delta)
+
+  second = t(A * (svd_current$d / (svd_current$d + lambda)))
+  res = first + second
+  data.table::setattr(res, "loss", loss)
+  res
+}
+
+# workhorse for soft_svd
+solve_iter_als_svd = function(x, svd_current, lambda, singular_vectors = c("u", "v")) {
+  singular_vectors = match.arg(singular_vectors)
+  m = (x %*% svd_current[[singular_vectors]]) %*% diag((svd_current$d / (svd_current$d + lambda)))
+  m
+}
+
+soft_als = function(x,
+                    rank = 10L, lambda = 0,
+                    n_iter = 100L, convergence_tol = 1e-3,
+                    init = NULL, final_svd = TRUE,
+                    target = c("svd", "soft_impute")) {
+
+  target = match.arg(target)
+  stopifnot(is.logical(final_svd) && length(final_svd) == 1)
+  tx = t(x)
+  if(is.null(init)) {
+    # draw random matrix and make columns orthogonal with QR decomposition
+    U = matrix(rnorm(n = nrow(x) * rank), nrow = nrow(x))
+    U = qr.Q(qr(U, LAPACK = TRUE))
+    # init with dummy values
+    D  = rep(1, rank)
+    V = matrix(rep(0, ncol(x) * rank), nrow = ncol(x))
+    svd_old = list(d = D, u = U, v = V); rm(U, V)
+  } else {
+    # warm start with another SVD
+    stopifnot(is.list(init))
+    stopifnot(all(names(init) %in% c("u", "d", "v")))
+    stopifnot(isTRUE(all.equal(dim(init$u), c(nrow(x), rank))))
+    stopifnot(isTRUE(all.equal(dim(init$v), c(ncol(x), rank))))
+    stopifnot(length(init$d) == rank)
+    svd_old = init
+  }
+
+  trace_iter = vector("list", n_iter)
+  k = 1L
+
+  svd_new = svd_old
+  CONVERGED = FALSE
+  for(i in seq_len(n_iter)) {
+    # Alternating algorithm
+    # 1. calculate for items
+    if(target == "soft_impute") {
+      B_hat = solve_iter_als_softimpute(tx, svd_new, lambda, "u")
+      Bsvd = reco:::svd_econ(B_hat %*% diag(sqrt(svd_new$d)))
+    } else if(target == "svd") {
+      B_hat = solve_iter_als_svd(tx, svd_new, lambda, "u")
+      Bsvd = reco:::svd_econ(B_hat)
+    }
+    rm(B_hat)
+    svd_new$v = Bsvd$u
+    svd_new$d = Bsvd$d
+    # not sure why this line is required
+    svd_new$u = svd_new$u %*% Bsvd$v
+
+    # 2. calculate for users
+    if(target == "soft_impute") {
+      A_hat = solve_iter_als_softimpute(x, svd_new, lambda, "v")
+      Asvd = reco:::svd_econ(A_hat %*% diag(sqrt(svd_new$d)))
+    } else if(target == "svd") {
+      A_hat = solve_iter_als_svd(x, svd_new, lambda, "v")
+      Asvd = reco:::svd_econ(A_hat)
+    } else {
+      stop(sprintf("unknown target = %s", target))
+    }
+    loss =  attr(A_hat, "loss")
+    rm(A_hat)
+
+    svd_new$u = Asvd$u
+    svd_new$d = Asvd$d
+    # not sure why this line is required
+    svd_new$v = svd_new$v %*% Asvd$v
+    rm(Asvd, Bsvd)
+    #log values of loss and change in frobenious norm
+    frob_delta = calc_frobenius_norm_delta(svd_old, svd_new)
+    trace_iter[[k]] = list(iter = i, scorer = "frob_delta", value = frob_delta)
+    k = k + 1L
+    if(!is.null(loss)) {
+      trace_iter[[k]] = list(iter = i, scorer = "loss", value = loss)
+      k = k + 1L
+      futile.logger::flog.debug("reco:::soft_impute: iter %03d, loss %.3f frobenious norm change %.3f",
+                                i, loss, frob_delta)
+    } else {
+      futile.logger::flog.debug("reco:::soft_impute: iter %03d, frobenious norm change %.3f",
+                                i, frob_delta)
+    }
+
+
+    svd_old = svd_new
+    # check convergence and
+    if(frob_delta < convergence_tol) {
+      futile.logger::flog.debug("reco:::soft_impute: converged with tol %f after %d iter", convergence_tol, i)
+      CONVERGED = TRUE
+      break
+    }
+  }
+  setattr(svd_new, "trace", data.table::rbindlist(trace_iter))
+  if(!CONVERGED)
+    futile.logger::flog.warn("reco:::soft_impute: didn't converged with tol %f after %d iterations - returning latest solution",
+                             convergence_tol, i)
+  if(final_svd) {
+    if(target == "soft_impute") {
+      A = t(svd_new$u) * sqrt(svd_new$d)
+      B = t(svd_new$v) * sqrt(svd_new$d)
+      x@x = x@x - make_sparse_approximation(x, A, B)
+      m = x %*% svd_new$v + t(A) %*% (B %*% svd_new$v)
+    } else if(target == "svd") {
+      m = x %*% svd_new$v
+    } else {
+      stop(sprintf("unknown target = %s", target))
+    }
+
+
+    m_svd = svd_econ(m)
+    final_singular_values = pmax(m_svd$d - lambda, 0)
+    n_nonzero_singular_values = sum(final_singular_values > 0)
+
+    m_svd = svd_econ(m)
+    final_singular_values = pmax(m_svd$d - lambda, 0)
+    n_nonzero_singular_values = sum(final_singular_values > 0)
+    if(n_nonzero_singular_values == 0) {
+      stop(sprintf("regularization lambda=%f is too high - all singular vectors are zero", lambda))
+    } else {
+      futile.logger::flog.info("final rank = %d", n_nonzero_singular_values)
+    }
+
+    svd_new = list(d = final_singular_values[seq_len(n_nonzero_singular_values)],
+                   u = m_svd$u[, seq_len(n_nonzero_singular_values)],
+                   v = tcrossprod(svd_new$v, m_svd$v)[, seq_len(n_nonzero_singular_values)])
+  }
+  svd_new
+}
