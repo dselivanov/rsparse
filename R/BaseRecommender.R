ea1972a25a18588e253ccf9d98c97293a119ed62
dselivanov <selivanov.dmitriy@gmail.com>
2017-12-10 18:29:14 +0400

    add item2item recommender

--- R/BaseRecommender.R
+++ R/BaseRecommender.R
@@ -12,6 +12,37 @@ BaseRecommender = R6::R6Class(
 
       user_embeddings = self$transform(x)
       private$predict_low_level(user_embeddings, private$components_, k, not_recommend)
+    },
+    get_similar_items = function(item_id, k = ncol(self$components), ... ) {
+      stopifnot(is.character(item_id) && length(item_id) == 1)
+      if(is.null(private$components_l2)) {
+        private$init_components_l2(...)
+      }
+      stopifnot()
+      if(is.null(private$item_ids)) {
+        stop("can't run 'get_similar_items()' - model doesn't have item ids (item_ids = NULL)")
+      }
+
+      i = which(colnames(private$components_l2) == item_id)
+      if(length(i) == 0) {
+        stop(sprintf("There is no item with id = '%s' in the model.", item_id))
+      }
+      query_embedding = private$components_l2[, i]
+      # dot-product to find cosine distance
+      # both components_l2 and query_embedding should have L2 norm = 1
+      # result is matrix with 1 row and n_items components
+      # scores = (query_embedding %*% private$components_l2[, -i, drop= FALSE])[1, ]
+      scores = (query_embedding %*% private$components_l2)
+      dim(scores) = NULL
+      # and also remove similarity with itself
+      scores = scores[-i]
+      ord = order(scores, decreasing = TRUE)
+      if(k < length(ord))
+        ord = ord[seq_len(k)]
+      res = private$item_ids[ord]
+      names(scores) = NULL
+      attr(res, "scores") = scores[ord]
+      res
     }
   ),
   private = list(
@@ -42,6 +73,16 @@ BaseRecommender = R6::R6Class(
       }
       indices
     },
-    item_ids = NULL
+
+    item_ids = NULL,
+    # prepare components
+    init_components_l2 = function(force_init = FALSE) {
+      if(is.null(private$components_l2) || force_init) {
+        flog.debug("calculating components_l2")
+        private$components_l2 = t(t(private$components_) / sqrt(colSums(private$components_ ^ 2)))
+      }
+    },
+    # L2 normalized components
+    components_l2 = NULL
   )
 )
