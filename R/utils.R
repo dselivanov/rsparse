1dd2e057bcf042f7a167e08c804a794bb1f41b72
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2019-06-09 16:33:23 +0400

    avoid blas/openmp thread contention in top_product

--- R/utils.R
+++ R/utils.R
@@ -28,6 +28,12 @@ train_test_split = function(x, test_proportion = 0.5) {
 
 
 find_top_product = function(x, y, k, not_recommend = NULL, exclude = integer(0), n_threads = getOption("rsparse_omp_threads", 1L)) {
+  n_threads_blas = RhpcBLASctl::blas_get_num_procs()
+  # set num threads to 1 in order to avoid thread contention between BLAS and openmp threads in `top_product()`
+  RhpcBLASctl::blas_set_num_threads(1L)
+  # restore on exit
+  on.exit(RhpcBLASctl::blas_set_num_threads(n_threads_blas))
+
   if(!inherits(exclude, "integer"))
     stop("'exclude' should be integer vector")
   if(!(is.null(not_recommend) || inherits(not_recommend, "sparseMatrix")))
