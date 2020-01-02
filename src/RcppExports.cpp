eef93145b9b6bfe7939c4c1a598402f1380fabb6
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2020-01-02 14:53:00 +0400

    bpr-mf

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -241,6 +241,26 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// bpr_solver_double
+void bpr_solver_double(const Rcpp::S4& m_csc_r, arma::Mat<double>& W, arma::Mat<double>& H, const arma::uword rank, const arma::uword n_updates, double learning_rate, double lambda_user, double lambda_item_positive, double lambda_item_negative, const arma::uword n_threads, bool update_items);
+RcppExport SEXP _rsparse_bpr_solver_double(SEXP m_csc_rSEXP, SEXP WSEXP, SEXP HSEXP, SEXP rankSEXP, SEXP n_updatesSEXP, SEXP learning_rateSEXP, SEXP lambda_userSEXP, SEXP lambda_item_positiveSEXP, SEXP lambda_item_negativeSEXP, SEXP n_threadsSEXP, SEXP update_itemsSEXP) {
+BEGIN_RCPP
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Rcpp::S4& >::type m_csc_r(m_csc_rSEXP);
+    Rcpp::traits::input_parameter< arma::Mat<double>& >::type W(WSEXP);
+    Rcpp::traits::input_parameter< arma::Mat<double>& >::type H(HSEXP);
+    Rcpp::traits::input_parameter< const arma::uword >::type rank(rankSEXP);
+    Rcpp::traits::input_parameter< const arma::uword >::type n_updates(n_updatesSEXP);
+    Rcpp::traits::input_parameter< double >::type learning_rate(learning_rateSEXP);
+    Rcpp::traits::input_parameter< double >::type lambda_user(lambda_userSEXP);
+    Rcpp::traits::input_parameter< double >::type lambda_item_positive(lambda_item_positiveSEXP);
+    Rcpp::traits::input_parameter< double >::type lambda_item_negative(lambda_item_negativeSEXP);
+    Rcpp::traits::input_parameter< const arma::uword >::type n_threads(n_threadsSEXP);
+    Rcpp::traits::input_parameter< bool >::type update_items(update_itemsSEXP);
+    bpr_solver_double(m_csc_r, W, H, rank, n_updates, learning_rate, lambda_user, lambda_item_positive, lambda_item_negative, n_threads, update_items);
+    return R_NilValue;
+END_RCPP
+}
 // top_product
 Rcpp::IntegerMatrix top_product(const arma::mat& x, const arma::mat& y, unsigned k, unsigned n_threads, const Rcpp::S4& not_recommend_r, const Rcpp::IntegerVector& exclude);
 RcppExport SEXP _rsparse_top_product(SEXP xSEXP, SEXP ySEXP, SEXP kSEXP, SEXP n_threadsSEXP, SEXP not_recommend_rSEXP, SEXP excludeSEXP) {
@@ -317,6 +337,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_rsparse_als_implicit_double", (DL_FUNC) &_rsparse_als_implicit_double, 8},
     {"_rsparse_als_implicit_float", (DL_FUNC) &_rsparse_als_implicit_float, 8},
     {"_rsparse_als_loss_explicit", (DL_FUNC) &_rsparse_als_loss_explicit, 5},
+    {"_rsparse_bpr_solver_double", (DL_FUNC) &_rsparse_bpr_solver_double, 11},
     {"_rsparse_top_product", (DL_FUNC) &_rsparse_top_product, 6},
     {"_rsparse_arma_kmeans", (DL_FUNC) &_rsparse_arma_kmeans, 6},
     {"_rsparse_omp_thread_count", (DL_FUNC) &_rsparse_omp_thread_count, 0},
---
+++ src/SolverBPRMF.cpp
@@ -0,0 +1,170 @@
+#include "rsparse.h"
+#define TRACK 100000
+
+arma::uword get_random_user(const arma::uword n_user);
+arma::uword get_positive_item(const arma::uvec &idx);
+arma::uword get_negative_item(const arma::uword n_item);
+//arma::uword get_negative_item(const dMappedCSR &x, const arma::uword u);
+arma::uvec positive_items(const dMappedCSR &x, const arma::uword u);
+
+// binary_search - check if element is in the sorted array
+// if yes - return true - skip sgd update
+bool skip_element(const arma::uvec &arr, arma::uword x) {
+  int l = 0;
+  int r = arr.size();
+  while (l <= r) {
+    size_t m = l + (r - l) / 2;
+
+    // Check if x is present at mid
+    if (arr[m] == x)
+      return true;
+
+    // If x greater, ignore left half
+    if (arr[m] < x)
+      l = m + 1;
+
+    // If x is smaller, ignore right half
+    else
+      r = m - 1;
+  }
+
+  // if we reach here, then element was
+  // not present
+  return false;
+}
+
+template <typename T> void bpr_solver(
+    const Rcpp::S4 &m_csc_r,
+    arma::Mat<T> &W, //user latent factors rank*n_user
+    arma::Mat<T> &H,  //item latent factors rank*n_item
+    const arma::uword rank,
+    const arma::uword n_updates,
+    T learning_rate,
+    // T momentum,
+    T lambda_user,
+    T lambda_item_positive,
+    T lambda_item_negative,
+    const  arma::uword n_threads,
+    bool update_items = true
+    ) {
+
+  const dMappedCSR x = extract_mapped_csr(m_csc_r);
+
+  const arma::uword n_user = x.n_rows;
+  const arma::uword n_item = x.n_cols;
+
+  // // accumulated gradients for momentum
+  // arma::Mat<T> W_grad(W.n_rows, W.n_cols, arma::fill::zeros);
+  // arma::Mat<T> H_grad(H.n_rows, H.n_cols, arma::fill::zeros);
+
+  #ifdef _OPENMP
+  #pragma omp parallel num_threads(n_threads)
+  #endif
+  {
+    size_t n_correct = 0, n_done = 0, n_skip = 0;
+    #ifdef _OPENMP
+    #pragma omp for schedule(guided, GRAIN_SIZE)
+    #endif
+    for(arma::uword iter = 0; iter < n_updates; ++iter) {
+      const arma::uword u = get_random_user(n_user);
+      const arma::uvec idx = positive_items(x, u);
+      const arma::uword i = get_positive_item(idx);
+      const arma::uword j = get_negative_item(n_item);
+
+      n_done++;
+      if (n_done % TRACK == TRACK - 1) {
+        #pragma omp critical
+        {
+          std::cout << "step " << iter + 1 << "/" << n_updates << " AUC:"<< (n_correct + 0.0) / (TRACK - n_skip) << " skip " << n_skip << std::endl;
+        }
+        n_correct = 0;
+        n_skip = 0;
+        n_done = 0;
+      }
+
+      bool skip = skip_element(idx, j);
+      // bool skip = false;
+      if(skip) {
+        n_skip++;
+      } else {
+        auto x_uij = W.col(u).t() * H.col(i) - W.col(u).t() * H.col(j);
+        T score = arma::as_scalar(1.0 / (1.0 + arma::exp(x_uij)));
+        if (score < .5) n_correct++;
+
+        // W_grad.col(u) = momentum * W_grad.col(u) + (1 - momentum) * (H.col(i) - H.col(j));
+        // H_grad.col(i) = momentum * H_grad.col(i) + (1 - momentum) * W.col(u);
+        // H_grad.col(j) = momentum * H_grad.col(j) - (1 - momentum) * W.col(u);
+        // auto d_xui_d_wu = W_grad.col(u);
+        // auto d_xui_d_hi = H_grad.col(i);
+        // auto d_xui_d_hj = H_grad.col(j);
+
+        arma::Col<T> d_xui_d_wu = H.col(i) - H.col(j);
+        arma::Col<T> d_xui_d_hi = W.col(u);
+        arma::Col<T> d_xui_d_hj = -W.col(u);
+
+        W.col(u) += learning_rate * (score * d_xui_d_wu + lambda_user * W.col(u));
+        if (update_items) {
+          H.col(i) += learning_rate * (score * d_xui_d_hi + lambda_item_positive * H.col(i));
+          H.col(j) += learning_rate * (score * d_xui_d_hj + lambda_item_negative * H.col(j));
+        }
+      }
+    }
+  }
+}
+
+inline arma::uword get_random_user(const arma::uword n_user) {
+  return(arma::randi( arma::distr_param(0, n_user - 1) ));
+}
+
+inline arma::uword get_negative_item(const arma::uword n_item) {
+  return(arma::randi( arma::distr_param(0, n_item - 1) ));
+}
+
+arma::uvec positive_items(const dMappedCSR &x, const arma::uword u) {
+  const arma::uword p1 = x.row_ptrs[u];
+  const arma::uword p2 = x.row_ptrs[u + 1];
+  arma::uvec idx = arma::uvec(&x.col_indices[p1], p2 - p1);
+  return(idx);
+}
+
+inline arma::uword get_positive_item(const arma::uvec &idx) {
+  arma::uword index_random_element = arma::randi( arma::distr_param(0, idx.size() - 1) );
+  return(idx[index_random_element]);
+}
+
+
+
+// [[Rcpp::export]]
+void bpr_solver_double(
+    const Rcpp::S4 &m_csc_r,
+    arma::Mat<double> &W, //user latent factors rank*n_user
+    arma::Mat<double> &H,  //item latent factors rank*n_item
+    const arma::uword rank = 8,
+    const arma::uword n_updates = 1e5,
+    double learning_rate = 0.01,
+    double lambda_user = 0.0,
+    double lambda_item_positive = 0.0,
+    double lambda_item_negative = 0.0,
+    const arma::uword n_threads = 1,
+    bool update_items = true
+) {
+  bpr_solver<double>(m_csc_r, W, H, rank, n_updates, learning_rate, lambda_user, lambda_item_positive, lambda_item_negative, n_threads, update_items);
+}
+
+/*
+ library("rsparse")
+ data("movielens100k")
+ x = as(movielens100k, "RsparseMatrix")
+ rank = 32
+ n_updates = 1e6
+ learning_rate = 0.1
+ momentum = 0.8
+ lambda_user = lambda_item_positive = lambda_item_negative = 0
+ n_threads = 6
+ update_items = TRUE
+ n_users = nrow(x)
+ n_items = ncol(x)
+ W = matrix(runif(rank * n_users), ncol = n_users)
+ H = matrix(runif(rank * n_items), ncol = n_items)
+ system.time(rsparse:::bpr_solver_double(x, W, H, rank, n_updates, learning_rate, lambda_user, lambda_item_positive, lambda_item_negative, n_threads, update_items))
+ */
