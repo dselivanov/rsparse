8e4e9eb1b3894e67c3cf4dea61eeacf01bb5a53b
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2020-01-06 16:20:02 +0400

    bprmf solver improvements

--- src/SolverBPRMF.cpp
+++ src/SolverBPRMF.cpp
@@ -1,14 +1,15 @@
 #include "rsparse.h"
 
-arma::uword get_random_user(const arma::uword n_user);
-arma::uword get_positive_item(const arma::uvec &idx);
-arma::uword get_negative_item(const arma::uword n_item);
-//arma::uword get_negative_item(const dMappedCSR &x, const arma::uword u);
-arma::uvec positive_items(const dMappedCSR &x, const arma::uword u);
+const arma::uword get_random_user(const arma::uword n_user);
+const arma::uword get_positive_item_index(const arma::uword max_index);
+const arma::uword get_negative_candidate(const arma::uword n_item);
+
+template <typename T> T positive_item_relevance(const MappedCSR<T> &x, const arma::uword u, const arma::uword i);
+template <typename T> const arma::uvec positive_items(const MappedCSR<T> &x, const arma::uword u);
 
 // binary_search - check if element is in the sorted array
 // if yes - return true - skip sgd update
-bool skip_element(const arma::uvec &arr, arma::uword x) {
+bool is_negative(const arma::uvec &arr, arma::uword x) {
   int l = 0;
   int r = arr.size();
   while (l <= r) {
@@ -16,7 +17,7 @@ bool skip_element(const arma::uvec &arr, arma::uword x) {
 
     // Check if x is present at mid
     if (arr[m] == x)
-      return true;
+      return false;
 
     // If x greater, ignore left half
     if (arr[m] < x)
@@ -29,27 +30,35 @@ bool skip_element(const arma::uvec &arr, arma::uword x) {
 
   // if we reach here, then element was
   // not present
-  return false;
+  return true;
+}
+
+template <typename T>
+T rank_loss(const T x) {
+  return std::log1p(x + 1);
 }
 
 template <typename T> void bpr_solver(
-    const dMappedCSR &x,
+    const MappedCSR<T> &x,
     arma::Mat<T> &W, //user latent factors rank*n_user
     arma::Mat<T> &H,  //item latent factors rank*n_item
     const arma::uword rank,
     const arma::uword n_updates,
-    T learning_rate,
-    T momentum,
-    T lambda_user,
-    T lambda_item_positive,
-    T lambda_item_negative,
+    const T learning_rate,
+    const T momentum,
+    const T lambda_user,
+    const T lambda_item_positive,
+    const T lambda_item_negative,
     const  arma::uword n_threads,
     bool update_items = true
     ) {
 
-  const arma::uword TRACK = n_updates / 100;
+
+  const arma::uword TRACK = n_updates / n_threads / 10;
   const arma::uword n_user = x.n_rows;
   const arma::uword n_item = x.n_cols;
+  // max_negative_samples = std::min(max_negative_samples, n_item);
+  // const double WARP_RANK_NORMALIZER = rank_loss(double(n_item));
 
   // accumulated gradients for momentum
   arma::Mat<T> W_grad(W.n_rows, W.n_cols, arma::fill::zeros);
@@ -59,51 +68,89 @@ template <typename T> void bpr_solver(
   #pragma omp parallel num_threads(n_threads)
   #endif
   {
-    size_t n_correct = 0, n_done = 0, n_skip = 0;
+    size_t n_correct = 0, n_positive = 0, n_negative = 0;
     #ifdef _OPENMP
-    //#pragma omp for schedule(guided, GRAIN_SIZE)
     #pragma omp for schedule(static, GRAIN_SIZE)
     #endif
     for(arma::uword iter = 0; iter < n_updates; ++iter) {
+
+      // T relevance = positive_item_relevance(x, u, id);
+      if (n_positive >= TRACK) {
+        if (is_master()) {
+          Rcpp::Rcout.precision(3);
+          Rcpp::Rcout << \
+            100 * (iter + 1.0) / n_updates << "%" << std::setw(10) << \
+            " AUC:"<< double(n_correct) / (n_negative) << std::endl;
+        }
+        n_correct = n_positive = n_negative = 0;
+      }
+
       const arma::uword u = get_random_user(n_user);
       const arma::uvec idx = positive_items(x, u);
 
-      if (idx.is_empty()) { // user with no positive items
-        continue;
-      }
+      if (idx.is_empty()) continue; // user with no positive items
+      auto id = get_positive_item_index(idx.size() - 1);
+      const arma::uword i = idx[id];
+      n_positive++;
 
-      const arma::uword i = get_positive_item(idx);
-      const arma::uword j = get_negative_item(n_item);
-
-      n_done++;
-      if (n_done % TRACK == TRACK - 1) {
-        if (is_master()) {
-          Rcpp::Rcout.precision(3);
-          Rcpp::Rcout << 100 * (iter + 1.0) / n_updates << "%" << std::setw(10) << \
-            " AUC:"<< (n_correct + 0.0) / (TRACK - n_skip) << std::setw(10) << \
-            " skip " << n_skip << std::endl;
-        }
-        n_correct = 0;
-        n_skip = 0;
-        n_done = 0;
-      }
-
-      bool skip = skip_element(idx, j);
+      // T diff = 0, score = 0;
+      // arma::uword j = get_negative_candidate(n_item);
+      // arma::Col<T> h_j;
       // bool skip = false;
-      if(skip) {
-        n_skip++;
-      } else {
+      // const double weight = 1;
+      // if (is_negative(idx, j)) {
+      //   n_negative++;
+      //   h_j = H.col(j);
+      //   diff = dot(w_u, h_i) - dot(w_u, h_j);
+      //   score = 1.0 / (1.0 + std::exp(diff));
+      //   if (score < .5) {
+      //     n_correct++;
+      //   }
+      // } else {
+      //   skip = true;
+      // }
+
+      // arma::Col<T> h_j;
+      //
+      // arma::uword j = 0, k = 0;
+      // bool skip = true;
+      // T diff = 0, score = 0;
+      // double weight = 0;
+      // for (k = 0; k < max_negative_samples; k++) {
+      //   j = get_negative_candidate(n_item);
+      //   // continue if we've sampled a really negative element
+      //   if (is_negative(idx, j)) {
+      //     n_negative++;
+      //     h_j = H.col(j);
+      //     diff = dot(w_u, h_i) - dot(w_u, h_j);
+      //     score = 1.0 / (1.0 + std::exp(diff));
+      //     if (score < .5) {
+      //       n_correct++;
+      //     }
+      //     // if (score > thresh) {
+      //     //   skip = false;
+      //     // }
+      //   }
+      //   if(!skip) {
+      //     // WARP weight
+      //     weight = rank_loss((double(n_item) - 1.0) / double(k + 1)) / WARP_RANK_NORMALIZER;
+      //     break;
+      //   }
+      // }
+
+      arma::uword j = get_negative_candidate(n_item);
+      if(is_negative(idx, j)) {
         const auto w_u = W.col(u);
         const auto h_i = H.col(i);
         const auto h_j = H.col(j);
 
-        T score = 1.0 / (1.0 + std::exp(dot(w_u, h_i) - dot(w_u, h_j)));
+        n_negative++;
+        const double weight = 1;
+        arma::Col<T> w_grad = weight * (h_i - h_j);
+        arma::Col<T> h_grad_i =  weight * w_u;
+        arma::Col<T> h_grad_j = -weight * w_u;
+        auto score = 1.0 / (1.0 + std::exp(dot(w_u, h_i) - dot(w_u, h_j)));
         if (score < .5) n_correct++;
-        if (score < .1 || score > .9) continue;
-
-        arma::Col<T> w_grad = h_i - h_j;
-        arma::Col<T> h_grad_i = w_u;
-        arma::Col<T> h_grad_j = -w_u;
 
         if (momentum > 0) {
           w_grad = momentum * W_grad.col(u) + (1 - momentum) * w_grad;
@@ -133,24 +180,31 @@ template <typename T> void bpr_solver(
   }
 }
 
-inline arma::uword get_random_user(const arma::uword n_user) {
+inline const arma::uword get_random_user(const arma::uword n_user) {
   return(arma::randi( arma::distr_param(0, n_user - 1) ));
 }
 
-inline arma::uword get_negative_item(const arma::uword n_item) {
+inline const arma::uword get_negative_candidate(const arma::uword n_item) {
   return(arma::randi( arma::distr_param(0, n_item - 1) ));
 }
 
-arma::uvec positive_items(const dMappedCSR &x, const arma::uword u) {
+template <typename T>
+T positive_item_relevance(const MappedCSR<T> &x, const arma::uword u, const arma::uword i) {
+  const arma::uword p1 = x.row_ptrs[u];
+  return(x.values[p1 + i]);
+}
+
+template <typename T>
+const arma::uvec positive_items(const MappedCSR<T> &x, const arma::uword u) {
   const arma::uword p1 = x.row_ptrs[u];
   const arma::uword p2 = x.row_ptrs[u + 1];
   arma::uvec idx = arma::uvec(&x.col_indices[p1], p2 - p1, false, true);
   return(idx);
 }
 
-inline arma::uword get_positive_item(const arma::uvec &idx) {
-  arma::uword index_random_element = arma::randi( arma::distr_param(0, idx.size() - 1) );
-  return(idx[index_random_element]);
+const arma::uword get_positive_item_index(const arma::uword max_index) {
+  arma::uword index_random_element = arma::randi( arma::distr_param(0, max_index) );
+  return(index_random_element);
 }
 
 
@@ -171,8 +225,11 @@ void bpr_solver_double(
     bool update_items = true
 ) {
   const dMappedCSR x = extract_mapped_csr(m_csc_r);
-  bpr_solver<double>(x, W, H, rank, n_updates, learning_rate, momentum, lambda_user, lambda_item_positive, lambda_item_negative, n_threads, update_items);
+  bpr_solver<double>(x, W, H, rank, n_updates, learning_rate, momentum, \
+                     lambda_user, lambda_item_positive, lambda_item_negative, \
+                     n_threads, update_items);
 }
+
 
 /*
  library("rsparse")
