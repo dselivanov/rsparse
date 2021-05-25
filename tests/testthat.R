a1ded196c097e53c0d690dce2b32bdef2218418d
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2021-05-25 17:01:10 +0400

    test scenario when input matrix has rows with only zeros

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,3 +8,9 @@ logger = lgr::get_logger('rsparse')
 logger$set_threshold('warn')
 data("movielens100k")
 test_check("rsparse")
+
+movielens100k = rbind(
+  movielens100k,
+  as(matrix(0, nrow = 2, ncol = ncol(movielens100k)), 'sparseMatrix'),
+  movielens100k[1:2, ]
+)
